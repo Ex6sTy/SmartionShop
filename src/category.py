0a98699cdc685c60d751f97ae02c9814fa05b455
Artemi Bogatov <aboga99@mail.ru>
2025-01-13 17:18:18 +0300

    Корретировка работы программы.

--- src/category.py
+++ src/category.py
@@ -2,9 +2,9 @@ from src.product import Product
 
 
 class Category:
-    name = str
-    description = str
-    products = str
+    name = str # Название категории
+    description = str # Описание категории
+    products = str # Продукты категории
     category_count = 0
     product_count = 0
 
@@ -16,13 +16,13 @@ class Category:
         Category.product_count += len(products) if products else 0
 
 if __name__ == "__main__":
-    product1 = Product("Apple iPhone 16 Pro Max 1Tb", "Смартфон Apple", 171900.00,13)
-    product2 = Product("Apple iPhone 16 Pro 256Gb", "Смартфон Apple", 120900.00,7)
-    product3 = Product("Apple iPhone 16 Pro 1Tb", "Смартфон Apple", 169500.00,25)
-    product4 = Product("Apple iPhone 16 128Gb", "Смартфон Apple", 83900.00,9)
+    product1 = Product("Смартфоны", "Устройства для связи", 171900.00,13)
+    product2 = Product("Компьютеры и ноутбуки", "Для работы и игр", 120900.00,7)
+    product3 = Product("Гаджеты", "Умные устройства", 169500.00,25)
+    product4 = Product("Телевизоры", "Устройства для отображения видео в высоком качестве")
+    product5 = Product("Инновационная техника", "Для 3D-печати", 83900.00,9)
 
-    category = Category("Смартфон", "Apple", [product1, product2, product3, product4])
-    category = Category("Смартфон", "Apple", [product1, product2, product3, product4])
+    category = Category("Смартфоны", "Apple", [product1, product2, product3, product4])
 
     print(category.name)
     print(category.description)
--- src/product.py
+++ src/product.py
@@ -5,14 +5,15 @@ class Product:
     quantity = int
 
     def __init__(self, name, description, price, quantity):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
+        self.name = name # Бренд
+        self.description = description # Описание товара
+        self.price = price #  Цена
+        self.quantity = quantity # Количество
 
 
 if __name__ == "__main__":
-    product = Product("Смартфон", "Позволяет звонить", 75000.00, 12)
+    product = Product("Apple iPhone 16 Pro Max", "Флагман с передовой камерой", 150000.00, 15)
+
 
     print(product.name)
     print(product.description)
